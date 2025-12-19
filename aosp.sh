872e407d4b96a78d2aa6eb92738c1b613fcc4a3e
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-12-19 08:20:28 +0530

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,16 +1,3 @@
-mkdir -p device/nothing/{galaga,galaga-kernel,dump}
-mkdir -p device/mediatek/sepolicy_vndr
-mkdir -p hardware/mediatek
-git clone https://github.com/sm455/android_device_nothing_Galaga -b los device/nothing/galaga
-git clone https://github.com/nothing-galaga/android_device_nothing_Galaga-kernel device/nothing/galaga-kernel
-git clone https://gitea.com/Saikrishna1504/galaga_dump device/nothing/dump
-git clone https://github.com/nothing-galaga/android_hardware_mediatek hardware/mediatek
-git clone https://github.com/nothing-galaga/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr 
-
-
 source build/envsetup.sh
-export WITH_GAPPS=true
-python device/nothing/galaga/extract-files.py device/nothing/galaga/dump 
-brunch galaga
-
-
+lunch lineage_Galaga-bp2a-user
+mka derp
